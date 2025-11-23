9a2afe1f1ec55ef35446dd35fdf1bbb119d2cae1
Ripcode1 <161825490+Ripcode1@users.noreply.github.com>
2025-11-23 22:43:52 +0200

    Update Layout.tsx

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,1 +1,15 @@
+import { LayoutProps } from "@/interfaces";
+import Footer from "./Footer";
+import Header from "./Header";
 
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="pt-24">{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
